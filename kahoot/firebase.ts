d5d743083a8fa92fb5d064b0f18bfa91ab7c0551
Aliyan Siddiqui <141032178+Aliyan555-555@users.noreply.github.com>
2025-04-09 18:00:41 +0500

    deploy-production

--- kahoot/firebase.ts
+++ kahoot/firebase.ts
@@ -1,16 +1,6 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBoDibJuzC93BeHIO90TZJhwuj5SIVomOg",
-//   authDomain: "kahootclone-7711d.firebaseapp.com",
-//   projectId: "kahootclone-7711d",
-//   storageBucket: "kahootclone-7711d.firebasestorage.app",
-//   messagingSenderId: "159155311275",
-//   appId: "1:159155311275:web:1e4904ea12b25a22b49dee",
-//   measurementId: "G-0ZYEZZK01M"
-// };
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAxQWdUXOCsQGkDOOd6QvQKDt9OCeJHTJg",
@@ -21,9 +11,11 @@ const firebaseConfig = {
   appId: "1:988978603806:web:7b4662f3b94538c1154b0f",
   measurementId: "G-B2YKD6DZ60"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const analytics = getAnalytics(app); // Initialize Google Analytics
 
 const googleProvider = new GoogleAuthProvider();
 
-export { auth,googleProvider };
+export { auth, googleProvider, analytics };
